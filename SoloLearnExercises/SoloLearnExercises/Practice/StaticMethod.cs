60819eac5545837d775381c761fb3ae8578662aa
Julia <JuliaAmaralSantos@outlook.com>
2024-08-07 19:34:37 -0300

    usando metodo estatico em diferentes objetos para verificar como ele se comporta

---
+++ SoloLearnExercises/SoloLearnExercises/Practice/StaticMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearnExercises.Practice
+{
+    public class StaticMethod
+    {
+        public class Cat
+        {
+            public static int count = 0;
+            public Cat()
+            {
+                count++;
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
--- SoloLearnExercises/SoloLearnExercises/Program.cs
+++ SoloLearnExercises/SoloLearnExercises/Program.cs
@@ -1,10 +1,22 @@
 ﻿using SoloLearnExercises.Exercise;
+using SoloLearnExercises.Practice;
+using static SoloLearnExercises.Practice.StaticMethod;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        StringMethod obj1 = new StringMethod();
-        obj1.OutputWords();
+        //StringMethod
+                        /*StringMethod obj1 = new StringMethod();
+                        obj1.OutputWords();
+                        */
+        //StringMethod
+
+
+        //StaticMethod
+        Cat kimi = new Cat();
+        Cat yumi = new Cat();
+        Cat kira = new Cat();
+        //StaticMethod
     }
 }
